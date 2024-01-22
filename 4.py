e525fca7e046d037d3f4fbfe2a65c115aaeab7e5
Jinal P. Vachheta <156422920+jinal-vachheta-58@users.noreply.github.com>
2024-01-22 19:20:06 +0530

    find simple interest

---
+++ 4.py
@@ -0,0 +1,24 @@
+# Python Program for simple interest
+#  principal amount, time and
+# rate of interest taken from user.
+ 
+ 
+def simple_interest(p,t,r):
+    print('The principal is', p)
+    print('The time period is', t)
+    print('The rate of interest is',r)
+     
+    si = (p * t * r)/100
+     
+    print('The Simple Interest is', si)
+     
+P = int(input("Enter the principal amount :"))
+T = int(input("Enter the time period :"))
+R = int(input("Enter the rate of interest :"))
+simple_interest(P,T,R)
+
+# OUTPUT:
+# The principal is 5000
+# The time period is 4
+# The rate of interest is 3
+# The Simple Interest is 600.0
