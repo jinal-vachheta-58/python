3726a881a525a323126ca7ca854c70f89d15f6d6
Jinal P. Vachheta <156422920+jinal-vachheta-58@users.noreply.github.com>
2024-01-22 19:18:14 +0530

    add two numbers

---
+++ 2.py
@@ -0,0 +1,12 @@
+# Python program to add two numbers
+
+num1 = int(input("Enter the number1 : "))
+num2 = int(input("Enter the number2 : "))
+
+sum = num1 + num2
+print("sum of two numbers",sum)
+
+# OUTPUT:
+# Enter the number1 : 3
+# Enter the number2 : 4
+# sum of two numbers 7
