5aad1f2fa3a48d4fd0c89e28234816e0ce93c9d7
Jinal P. Vachheta <156422920+jinal-vachheta-58@users.noreply.github.com>
2024-01-23 16:59:19 +0530

    python variables

---
+++ 6.py
@@ -0,0 +1,18 @@
+# python variables
+# python variable can be reinitialize
+# no need to declare data types
+# in every reinitilization of variable will change it's id 
+a = "jinal"
+print(id(a)) 
+
+a = "aasima"
+print(id(a)) 
+
+a = 10
+print(id(a)) 
+
+# output:
+# PS C:\Users\Mcs258\Desktop\python j> py 7.py
+# 2435156505584
+# 2435156505264
+# 140725801968712
