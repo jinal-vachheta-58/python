181a8a3e8ad985c4a91c976a5979de33d1a6665e
Jinal P. Vachheta <156422920+jinal-vachheta-58@users.noreply.github.com>
2024-01-23 16:58:39 +0530

    if else in python

---
+++ 5.py
@@ -0,0 +1,13 @@
+# if else 
+a = 10
+b = 20
+
+if a > b :
+    print("a is greater than b")
+elif a < b :
+    print("a is less than b")
+else:
+    print("a and b sre equal")
+
+# OUTPUT:
+# a is less than b
