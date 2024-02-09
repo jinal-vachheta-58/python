222d287c1cfe2971cd017e8252bc297355c18f5c
Jinal P. Vachheta <156422920+jinal-vachheta-58@users.noreply.github.com>
2024-02-09 12:47:21 +0530

    add two numbers

---
+++ python_1_.py
@@ -0,0 +1,16 @@
+def add_numbers(x,y):
+    return x+y
+
+
+
+x = int(input("enter first number : "))
+y = int(input("enter second number : "))
+
+result = add_numbers(x,y)
+print("sum of x and y = ",result)
+
+# output:
+# PS C:\Users\rcc\Desktop\python assignment> python python_1_.py 
+# enter first number : 5
+# enter second number : 9
+# sum of x and y =  14
