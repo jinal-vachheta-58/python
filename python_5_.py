52e5f74b2faf099bcaaa045a67d6281dffeb6565
Jinal P. Vachheta <156422920+jinal-vachheta-58@users.noreply.github.com>
2024-03-07 01:19:35 +0530

    check whether number is even or odd

---
+++ python_5_.py
@@ -0,0 +1,12 @@
+#5) Write a python program to check whether number is even or odd.
+
+a = int(input("Enter num for a : "))
+
+if((a % 2) == 0):
+    print(a," : a is even")
+else:
+    print(a," : a is odd")
+
+#OUTPUT:
+#Enter num for a : 8
+#8  : a is even
