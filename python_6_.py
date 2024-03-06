5af036a8c50e16a5e7244fd46baf77f0790b9136
Jinal P. Vachheta <156422920+jinal-vachheta-58@users.noreply.github.com>
2024-03-07 01:20:02 +0530

    check whether a character is vowel or consonant

---
+++ python_6_.py
@@ -0,0 +1,12 @@
+#6) Write a python program to check whether a character is vowel or consonant.
+
+char = input("Enter character for a : ")
+
+if char in ['A','E','I','O','U','a','e','i','o','u']:
+    print("character is vowel")
+else:
+    print("character is consonant")
+
+#OUTPUT:
+#Enter character for a : r
+#character is consonant
