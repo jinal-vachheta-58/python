4d5372f550da98180743f1e89df8cc912534bcad
Jinal P. Vachheta <156422920+jinal-vachheta-58@users.noreply.github.com>
2024-03-07 01:18:30 +0530

    display the greatest number.

---
+++ python_4_.py
@@ -0,0 +1,14 @@
+#4) Write a python program to read two numbers and display the greatest number.
+
+a = int(input("Enter num for a : "))
+b = int(input("Enter num for b : "))
+
+if(a > b):
+    print(a," : a is greater")
+else:
+    print(b," : b is greater")
+
+#OUTPUT:
+ #   Enter num for a : 56
+  #  Enter num for b : 89
+   # 89  : b is greater
