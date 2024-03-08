0e409aae788abc5aad4c6897239cfc0d4a892587
Jinal P. Vachheta <156422920+jinal-vachheta-58@users.noreply.github.com>
2024-03-08 20:21:03 +0530

    calculate sum of natural numbers

---
+++ python_9_.py
@@ -0,0 +1,13 @@
+#9) Write a python program to calculate sum of natural numbers
+
+array = [5,-5,0,3,8,-2]
+total = 0
+
+for a in array:
+    if(a > 0):
+        total  = total + a
+        
+print("total of natural numbers : " ,total)
+        
+#OUTPUT:
+#total of natural numbers :  16
