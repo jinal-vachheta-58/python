9e60ff1006293ab52aee2d64118e4f25f37f29b8
Jinal P. Vachheta <156422920+jinal-vachheta-58@users.noreply.github.com>
2025-01-16 15:23:27 +0530

    string functions like reverse, replace, sublist

---
+++ slicing.py
@@ -0,0 +1,62 @@
+#1
+'''
+def odd_Even(l):
+    
+    print(l)
+    odd= l[::2]
+    print(odd)
+    
+    even=l[1::2]
+    print(even)
+    
+    
+l=["aaima","adku","kahgdg","jhd","flkd","kdhas","lfhks"]
+odd_Even(l)
+'''
+
+
+#2
+'''
+def reverse(l):
+    r =l[::-1]
+    print(r)
+    
+    
+l=["aaima","adku","kahgdg","jhd","flkd","kdhas","lfhks"]
+reverse(l)
+'''
+
+#3
+'''
+
+def suvlist(l):
+    sub=l[2:6]
+    print(sub)
+
+    
+l=["aaima","adku","kahgdg","jhd","flkd","kdhas","lfhks"]
+suvlist(l)
+'''
+
+
+'''
+#4
+def alter(l):
+    sub=l[::2]
+    print(sub)
+
+    
+l=["aaima","adku","kahgdg","jhd","flkd","kdhas","lfhks"]
+alter(l)
+
+'''
+'''
+#5
+def replace(l):
+   l[2:3]="kha"
+   print(l)
+   
+l=["aaima","adku","kahgdg","jhd","flkd","kdhas","lfhks"]
+replace(l)
+
+'''
